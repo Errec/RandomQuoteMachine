32cb73bd8e69fceafe4ad3f6eebd50aa37ee900e
Raniro Coelho <errecme@gmail.com>
2016-09-02 13:55:59 -0300

    Tweet intent setup

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+var quote;
+var author;
+var urlBase = "https://twitter.com/intent/tweet?text=";
+
 $(document).ready(function(){
   $(".btn").click(function(){
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(getQuote) {
-      $("#quote").html(getQuote[0].content);
-      $("#author").text(getQuote[0].title);
+      quote = getQuote[0].content;
+      author = getQuote[0].title;
+
+      $("#quote").html(quote);
+      $("#author").text(author);
+      $("a[href^='https://twitter.com/intent/tweet?text=']").prop("href", urlBase + $("#quote").text().trim() + "%0D%0A" + $("#author").text().trim())
     });
   });
 });
+
+
+
+      // twttr.widgets.load();
+
+
+
+
+
+
