d53956535bb92e83a8b51bca8b57983129715e13
Raniro Coelho <errecme@gmail.com>
2016-09-03 19:51:24 -0300

    encapsulate js function, load quote with pageload, set css initial color scheme

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-var quote;
-var author;
 var urlBase = "https://twitter.com/intent/tweet?text=";
 
-$(document).ready(function(){
-  $(".btn").click(function(){
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(getQuote) {
-      quote = getQuote[0].content;
-      author = getQuote[0].title;
+/*
+* This function trim the extra white space and set quote and author as tweet text filling the Twitter intent field "text="
+*/
+function tweetText() {
+    $("a[href^='https://twitter.com/intent/tweet?text=']").prop("href", urlBase + $("#quote").text().trim() + "%0D%0A" + $("#author").text().trim());
+}
 
-      $("#quote").html(quote);
-      $("#author").text(author);
-      $("a[href^='https://twitter.com/intent/tweet?text=']").prop("href", urlBase + $("#quote").text().trim() + "%0D%0A" + $("#author").text().trim())
-    });
+/*
+* This function get the Json from quotesondesign.com API and inflates the HTML with quote and author text
+*/
+function getNewQuote() {
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(getQuote) {
+  $("#quote").html(getQuote[0].content);
+  $("#author").text(getQuote[0].title);
+  tweetText();
   });
+}
+
+$(document).ready(function(){
+  getNewQuote();
+  $('.btn').on('click', getNewQuote);
 });
 
-
-
-      // twttr.widgets.load();
 
 
 
